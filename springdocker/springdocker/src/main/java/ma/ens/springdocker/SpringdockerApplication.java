c795b4a3426751100344a07e7b3e3bab81f9f904
SALMA BENBOUKHRIS <salmabenboukhris@gmail.com>
2026-03-24 00:06:48 +0100

    commit

---
+++ springdocker/springdocker/src/main/java/ma/ens/springdocker/SpringdockerApplication.java
@@ -0,0 +1,11 @@
+package ma.ens.springdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdockerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdockerApplication.class, args);
+	}
+}
